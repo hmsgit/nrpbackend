d6235928f3588947173fdb107f44804a074ef4a0
Georg Hinkel <hinkel@fzi.de>
2014-12-01 13:00:55 +0100

    Added checkout of CLE repository to verification since we rely on Python modules that are generated using catkin.
    Change-Id: I19f0843e8f0834291f9137a5de566eeb54894830

--- verify.sh
+++ verify.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
-export IGNORE_LINT="platform_venv|generated|build"
+module load boost/1.55zlib-rhel6-x86_64-gcc4.4
+module load ros/hydro-rhel6-x86_64-gcc4.4
+source $ROS_SETUP_FILE
+
+virtualenv build_venv
+. build_venv/bin/activate
+pip install catkin_pkg
+
+if [ ! -d CLE ]; then \
+	git clone ssh://bbprelman@bbpcode.epfl.ch/neurorobotics/CLE
+fi;\
+
+# Installing the ros package to control gazebo
+cd CLE/GazeboRosPackage/src/
+catkin_init_workspace
+cd ..
+catkin_make
+source devel/setup.bash
+# catkin_make install
+cd ../..
+
+deactivate
+
+export IGNORE_LINT="platform_venv|generated|build|CLE"
 rm hbp_nrp_backend/hbp_nrp_backend/bibi_config/tests/generated_cle_script.py
 rm hbp_nrp_backend/hbp_nrp_backend/exd_config/tests/experiment.py
 rm hbp_nrp_backend/hbp_nrp_backend/exd_config/tests/experiment_bibi.py
