937af02f04905e531bd0fe08c10bb0d24dc8350c
cauli <cauli@bbpcode.epfl.ch>
2014-12-04 13:57:08 +0000

    state machine and test modified
    Change-Id: I8fbb59cc265b243926096cc9cc20670cfef0a722

--- hbp_nrp_backend/hbp_nrp_backend/simulation_control/transitions.py
+++ hbp_nrp_backend/hbp_nrp_backend/simulation_control/transitions.py
@@ -5,10 +5,10 @@ This file loads the production transitions
 
 __author__ = 'GeorgHinkel'
 
-import os
-from hbp_nrp_backend.exd_config import generate_experiment
-from hbp_nrp_backend.simulation_control import simulations
-import imp
+# import os
+# from hbp_nrp_backend.exd_config import generate_experiment
+# from hbp_nrp_backend.simulation_control import simulations
+# import imp
 
 
 def start_simulation(sim_id):
@@ -16,15 +16,17 @@ def start_simulation(sim_id):
     Starts the simulation with the given id
     :param sim_id: The simulation id
     """
-    simulation = simulations[sim_id]
-    directory = os.path.split(__file__)[0]
-    experiment = simulation.experiment_id
-    target = os.path.join(directory, '__generate_experiment.py')
-    generate_experiment(experiment, target)
+#    simulation = simulations[sim_id]
+#    directory = os.path.split(__file__)[0]
+#    experiment = simulation.experiment_id
+#    target = os.path.join(directory, '__generate_experiment.py')
+#    generate_experiment(experiment, target)
 
-    experiment_start = imp.load_source('experiment.setup', target)
-    experiment_start.start()
-    simulation.cle = experiment_start.cle
+#    experiment_start = imp.load_source('experiment.setup', target)
+#    experiment_start.start()
+#    simulation.cle = experiment_start.cle
+
+    print "Start simulation " + str(sim_id)
 
 
 def pause_simulation(sim_id):
@@ -32,15 +34,15 @@ def pause_simulation(sim_id):
     Pauses the simulation with the given id
     :param sim_id: The simulation id
     """
-    print "Pause simulation" + str(sim_id)
+    print "Pause simulation " + str(sim_id)
 
 
-def resume_simulation(sim_id):
+def reset_simulation(sim_id):
     """
     Resumes the simulation with the given simulation id
     :param sim_id: The simulation id
     """
-    print "Resume simulation" + str(sim_id)
+    print "Reset simulation " + str(sim_id)
 
 
 def stop_simulation(sim_id):
@@ -48,12 +50,12 @@ def stop_simulation(sim_id):
     Stops the simulation with the given id
     :param sim_id: The simulation id
     """
-    print "Stop simulation" + str(sim_id)
+    print "Stop simulation " + str(sim_id)
 
 
-def release_simulation(sim_id):
+def initialize_simulation(sim_id):
     """
     Releases the simulation with the given id
     :param sim_id: The simulation id
     """
-    print "Release simulation" + str(sim_id)
+    print "Initialize simulation " + str(sim_id)
