c537c00e572fa74d67b784d323b3ae0cf0872976
Daniel Peppicelli <daniel.peppicelli@gmail.com>
2015-01-23 11:41:35 +0100

    Do not release when not asked to do
    Change-Id: Icd90dd834f4d6ece1d3299098014a06e6a53ff23

--- verify.sh
+++ verify.sh
@@ -1,5 +1,39 @@
 #!/bin/bash
 
+# ------------------------------------------------------------------
+# Simple wrapper around the "make verify" target of the common hbp 
+# Makefile for python. This is mainly use for local development.
+# ------------------------------------------------------------------
+
+read -d '' USAGE << EOF
+Usage: ./verify.sh -hr\n
+r     Try to release the code to HBP pypy server\n
+h     Print usage
+
+EOF
+
+while getopts ":rh" optname
+  do
+    case "$optname" in
+      "r")
+        $RELEASE=true
+        ;;
+      "h")
+        echo "$USAGE"
+        exit 0;
+        ;;
+      "?")
+        echo "Unknown option $OPTARG"
+        echo "$USAGE"
+        exit 0;
+        ;;
+      *)
+        echo "Unknown error while processing options"
+        exit 0;
+        ;;
+    esac
+  done
+
 source GazeboRosPackage/GazeboRosPackage/install/setup.bash
 
 export IGNORE_LINT="platform_venv|hbp_nrp_backend/hbp_nrp_backend/bibi_config/generated|hbp_nrp_backend/hbp_nrp_backend/exd_config/generated|generated|build|CLE|hbp-flask-restful-swagger-master|GazeboRosPackage"
@@ -7,7 +41,11 @@ rm hbp_nrp_backend/hbp_nrp_backend/bibi_config/tests/generated_cle_script.py
 rm hbp_nrp_backend/hbp_nrp_backend/exd_config/tests/experiment.py
 rm hbp_nrp_backend/hbp_nrp_backend/exd_config/tests/experiment_bibi.py
 
-make verify
+if [ "$RELEASE" = true ] ; then
+    make verify
+else
+	make verify_base
+fi
 VERIFY_RET=$?
 
 rm -rf GazeboRosPackage
