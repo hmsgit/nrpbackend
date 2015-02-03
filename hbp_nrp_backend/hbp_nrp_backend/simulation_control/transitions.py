d1ae0fe05ff759650d5d48c807ecabf366471739
cauli <cauli@bbpcode.epfl.ch>
2015-02-03 13:52:44 +0000

    adding the log messages
    Change-Id: I5ef46de27c4390973ef0803414a179223bb7eefc

--- hbp_nrp_backend/hbp_nrp_backend/simulation_control/transitions.py
+++ hbp_nrp_backend/hbp_nrp_backend/simulation_control/transitions.py
@@ -8,6 +8,9 @@ __author__ = 'GeorgHinkel'
 from hbp_nrp_backend.exd_config import generate_bibi, initialize_experiment
 from hbp_nrp_backend.simulation_control import simulations
 import os
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def start_simulation(sim_id):
@@ -17,6 +20,7 @@ def start_simulation(sim_id):
     """
     simulation = simulations[sim_id]
     simulation.cle.start()
+    logger.info("simulation started")
 
 
 def pause_simulation(sim_id):
@@ -26,6 +30,7 @@ def pause_simulation(sim_id):
     """
     simulation = simulations[sim_id]
     simulation.cle.pause()
+    logger.info("simulation paused")
 
 
 def reset_simulation(sim_id):
@@ -35,6 +40,7 @@ def reset_simulation(sim_id):
     """
     simulation = simulations[sim_id]
     simulation.cle.reset()
+    logger.info("simulation reset")
 
 
 def stop_simulation(sim_id):
@@ -44,6 +50,7 @@ def stop_simulation(sim_id):
     """
     simulation = simulations[sim_id]
     simulation.cle.stop()
+    logger.info("simulation stopped")
 
 
 def initialize_simulation(sim_id):
@@ -55,9 +62,13 @@ def initialize_simulation(sim_id):
     simulation = simulations[sim_id]
     experiment = simulation.experiment_id
     models_path = os.environ.get('NRP_MODELS_DIRECTORY')
+    logger.debug("The NRP_MODELS_DIRECTORY is: %s", models_path)
     if models_path is not None:
         experiment = os.path.join(models_path, experiment)
+    else:
+        logger.warn("NRP_MODELS_DIRECTORY is empty")
     target = '__generated_experiment.py'
 
     generate_bibi(experiment, target)
     simulation.cle = initialize_experiment(experiment, target)
+    logger.info("simulation initialized")
