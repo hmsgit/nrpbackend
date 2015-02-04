d89d749644c1bf8a31809e0141c717fd4adb717e
Stefan Deser <deser@in.tum.de>
2015-02-04 16:06:15 +0100

    [NRRPLT-1390] improved runserver.py unit test
    Change-Id: Icfadb7f51aa25b13e7e5bc18f15a681c851f5749

--- hbp_nrp_backend/hbp_nrp_backend/tests/test_runserver.py
+++ hbp_nrp_backend/hbp_nrp_backend/tests/test_runserver.py
@@ -1,36 +1,51 @@
 """
 This class tests the main startup script.
 """
-__author__ = 'StefanDeser'
+__author__ = 'Stefan Deser'
 
 from argparse import Namespace
 from hbp_nrp_backend import runserver
-from mock import MagicMock
+import logging
+from mock import patch, MagicMock
+import os
 from testfixtures import log_capture
 import unittest
 
+
 class TestScript(unittest.TestCase):
+
     def setUp(self):
         # a simple mock for the application server
         class MockApp: pass
-        self.app = MockApp()
-        self.app.run = MagicMock()
+        self._app = MockApp()
+        self._app.run = MagicMock()
 
-        self.args = Namespace()
-        self.args.logfile = None
-        self.args.port = None
+        self._args = Namespace()
+        self._args.logfile = None
+        self._args.port = None
 
-    @log_capture()
+    def tearDown(self):
+        # remove all handlers after each test!
+        logging.getLogger(runserver.__name__).handlers = []
+
+    @log_capture(level=logging.WARNING)
     def test_run_server_no_arguments(self, logcapture):
-        runserver.run_server(self.app, self.args)
+        runserver.run_server(self._app, self._args)
         logcapture.check(
-                         ('hbp_nrp_backend.runserver', 'WARNING', 'Could not write to specified logfile or no logfile specified, logging to stdout now!'),
-                         ('hbp_nrp_backend.runserver', 'WARNING', 'Could not parse port, will use default port: ' + str(runserver.DEFAULT_PORT)),
-                         ('hbp_nrp_backend.runserver', 'INFO', 'Starting the REST backend server now ...'),
-                         ('hbp_nrp_backend.runserver', 'INFO', 'REST backend server terminated.')
+                         (runserver.__name__, 'WARNING', 'Could not write to specified logfile or no logfile specified, logging to stdout now!'),
+                         (runserver.__name__, 'WARNING', 'Could not parse port, will use default port: ' + str(runserver.DEFAULT_PORT))
                          )
-        self.assertTrue(self.app.run.called)
-        self.app.run.assert_called_with(port=runserver.DEFAULT_PORT, host=runserver.DEFAULT_HOST)
+        self.assertTrue(self._app.run.called)
+        self._app.run.assert_called_with(port=runserver.DEFAULT_PORT, host=runserver.DEFAULT_HOST)
+
+    def test_run_server_create_logfile(self):
+        # create a logfile in the current working directory
+        self._args.logfile = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'logfile.log')
+        runserver.run_server(self._app, self._args)
+
+        # check if the file exists
+        self.assertTrue(os.path.isfile(self._args.logfile))
+        os.remove(self._args.logfile)
 
 if __name__ == '__main__':
     unittest.main()
