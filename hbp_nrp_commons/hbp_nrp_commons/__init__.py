b2176000bd8097a0b7d12c5d3e84bc9db5f3fb53
Bernd Eckstein <eckstein@fzi.de>
2015-12-15 14:43:58 +0100

    hbp_nrp_commons fix and bump to 0.4.2.dev3
    Change-Id: Ice3812fd29a16955c8123174b1bb02fbf848e79e

--- hbp_nrp_commons/hbp_nrp_commons/__init__.py
+++ hbp_nrp_commons/hbp_nrp_commons/__init__.py
@@ -6,5 +6,5 @@ __author__ = "Georg Hinkel"
 
 from hbp_nrp_commons.version import VERSION as __version__  # pylint: disable=W0611
 
-from hbp_nrp_commons.generated import exp_conf_api_gen as ExperimentConfiguration
-from hbp_nrp_commons.generated import hbp_scxml_gen as ScXML
+#from hbp_nrp_commons.generated import exp_conf_api_gen as ExperimentConfiguration
+#from hbp_nrp_commons.generated import hbp_scxml_gen as ScXML
