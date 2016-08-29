cf45a4da71ed1e3d881c7e1613f5d8fe4a49fca7
Kenny Sharma <kenny.sharma@tum.de>
2016-08-29 08:47:16 +0200

    [NRRPLT-2739] Upgrade test model to PyNN 0.8.1
    Change-Id: I375d45456bbd481ed7e0822f4f6e32f3302b5fda

--- hbp_nrp_cleserver/hbp_nrp_cleserver/tests/server/brain_model/braitenberg.py
+++ hbp_nrp_cleserver/hbp_nrp_cleserver/tests/server/brain_model/braitenberg.py
@@ -6,49 +6,31 @@ This file contains the setup of the neuronal network running the Husky experimen
 
 __author__ = 'Lazar Mateev, Georg Hinkel'
 
-import hbp_nrp_cle.tf_framework as nrp
-import logging
-import pyNN.nest as sim
+from hbp_nrp_cle.brainsim import simulator as sim
 import numpy as np
-from pyNN.nest import *
+import logging
 
-
-nest.SetKernelStatus({'dict_miss_is_error': False})
 logger = logging.getLogger(__name__)
+
 
 def create_brain():
     """
     Initializes PyNN with the neuronal network that has to be simulated
     """
-    sim.setup(timestep=0.1, min_delay=0.1, max_delay=20.0, threads=1, debug=True)
-    C_m = 25.0
-    g_L = 2.5
-    t_m = C_m / g_L
-
-    SENSORPARAMS = {'a': 0.0,
-                    'b': 0.0,
-                    'delta_T': 0.0,
-                    'tau_w': 10.0,
-                    'v_spike': 0.0,
-                    'cm': C_m,
-                    'v_rest': -60.5,
-                    'tau_m': t_m,
-                    'e_rev_E': 0.0,
-                    'e_rev_I': -75.0,
-                    'v_reset': -60.5,
-                    'v_thresh': -60.0,
+    SENSORPARAMS = {'v_rest': -60.5,
+                    'cm': 0.025,
+                    'tau_m': 10.,
                     'tau_refrac': 10.0,
                     'tau_syn_E': 2.5,
-                    'tau_syn_I': 2.5}
+                    'tau_syn_I': 2.5,
+                    'e_rev_E': 0.0,
+                    'e_rev_I': -75.0,
+                    'v_thresh': -60.0,
+                    'v_reset': -60.5}
 
-    GO_ON_PARAMS = {'a': 0.0,
-                    'b': 0.0,
-                    'delta_T': 0.0,
-                    'tau_w': 10.0,
-                    'v_spike': 0.0,
-                    'cm': C_m,
-                    'v_rest': -60.5,
-                    'tau_m': t_m,
+    GO_ON_PARAMS = {'v_rest': -60.5,
+                    'cm': 0.025,
+                    'tau_m': 10.0,
                     'e_rev_E': 0.0,
                     'e_rev_I': -75.0,
                     'v_reset': -61.6,
@@ -57,108 +39,68 @@ def create_brain():
                     'tau_syn_E': 2.5,
                     'tau_syn_I': 2.5}
 
-    cells = sim.Population(8, sim.IF_cond_alpha)
-    params = {'U': 1.0, 'tau_rec': 0.0, 'tau_facil': 0.0}
-    syndynamics = sim.SynapseDynamics(fast=sim.TsodyksMarkramMechanism(**params))
+    # POPULATION_PARAMS = SENSORPARAMS * 5 + GO_ON_PARAMS + SENSORPARAMS * 2
+
+    population = sim.Population(8, sim.IF_cond_alpha())
+    population[0:5].set(**SENSORPARAMS)
+    population[5:6].set(**GO_ON_PARAMS)
+    population[6:8].set(**SENSORPARAMS)
+
+    # Shared Synapse Parameters
+    syn_params = {'U': 1.0, 'tau_rec': 1.0, 'tau_facil': 1.0}
 
     # Synaptic weights
     WEIGHT_RED_TO_ACTOR = 1.5e-4
-    WEIGHT_RED_TO_GO_ON = 1.2e-3
+    WEIGHT_RED_TO_GO_ON = 1.2e-3  # or -1.2e-3?
     WEIGHT_GREEN_BLUE_TO_ACTOR = 1.05e-4
     WEIGHT_GO_ON_TO_RIGHT_ACTOR = 1.4e-4
     DELAY = 0.1
 
     # Connect neurons
-    CONN = sim.AllToAllConnector(weights=abs(WEIGHT_RED_TO_ACTOR),
-                                 delays=DELAY)
-    sim.Projection(cells[2:3], cells[7:8], CONN, synapse_dynamics=syndynamics, target='excitatory')
-    sim.Projection(cells[3:4], cells[6:7], CONN, synapse_dynamics=syndynamics, target='excitatory')
+    CIRCUIT = population
 
-    CONN = sim.AllToAllConnector(weights=abs(WEIGHT_RED_TO_GO_ON),
-                                 delays=DELAY)
-    sim.Projection(cells[0:2], cells[5:6], CONN, synapse_dynamics=syndynamics, target='inhibitory')
+    SYN = sim.TsodyksMarkramSynapse(weight=abs(WEIGHT_RED_TO_ACTOR),
+                                    delay=DELAY, **syn_params)
+    sim.Projection(presynaptic_population=CIRCUIT[2:3],
+                   postsynaptic_population=CIRCUIT[7:8],
+                   connector=sim.AllToAllConnector(),
+                   synapse_type=SYN,
+                   receptor_type='excitatory')
+    sim.Projection(presynaptic_population=CIRCUIT[3:4],
+                   postsynaptic_population=CIRCUIT[6:7],
+                   connector=sim.AllToAllConnector(),
+                   synapse_type=SYN,
+                   receptor_type='excitatory')
 
-    CONN = sim.AllToAllConnector(weights=abs(WEIGHT_GREEN_BLUE_TO_ACTOR),
-                                 delays=DELAY)
-    sim.Projection(cells[4:5], cells[7:8], CONN, synapse_dynamics=syndynamics, target='excitatory')
+    SYN = sim.TsodyksMarkramSynapse(weight=abs(WEIGHT_RED_TO_GO_ON),
+                                    delay=DELAY, **syn_params)
+    sim.Projection(presynaptic_population=CIRCUIT[0:2],
+                   postsynaptic_population=CIRCUIT[5:6],
+                   connector=sim.AllToAllConnector(),
+                   synapse_type=SYN,
+                   receptor_type='inhibitory')
 
-    CONN = sim.AllToAllConnector(weights=abs(WEIGHT_GO_ON_TO_RIGHT_ACTOR),
-                                 delays=DELAY)
-    sim.Projection(cells[5:6], cells[7:8], CONN, synapse_dynamics=syndynamics, target='excitatory')
+    SYN = sim.TsodyksMarkramSynapse(weight=abs(WEIGHT_GREEN_BLUE_TO_ACTOR),
+                                    delay=DELAY, **syn_params)
+    sim.Projection(presynaptic_population=CIRCUIT[4:5],
+                   postsynaptic_population=CIRCUIT[7:8],
+                   connector=sim.AllToAllConnector(),
+                   synapse_type=SYN,
+                   receptor_type='excitatory')
 
+    SYN = sim.TsodyksMarkramSynapse(weight=abs(WEIGHT_GO_ON_TO_RIGHT_ACTOR),
+                                    delay=DELAY, **syn_params)
+    sim.Projection(presynaptic_population=CIRCUIT[5:6],
+                   postsynaptic_population=CIRCUIT[7:8],
+                   connector=sim.AllToAllConnector(),
+                   synapse_type=SYN,
+                   receptor_type='excitatory')
 
-    formatted_circuit = {
-        "population": cells,
-        "x": np.float64(np.array([0, 1, 2, 3, 4, 5, 6, 7])),
-        "y": np.float64(np.array([0, 1, 2, 3, 4, 5, 6, 7])),
-        "z": np.float64(np.array([0, 1, 2, 3, 4, 5, 6, 7])),
-        "layer": np.int16(np.array([1, 1, 1, 1, 1, 2, 3, 3])),
-        "mtype": np.array(['SC', 'SC', 'SC', 'SC', 'SC', 'GOC', 'AC', 'AC']),
-        "a": np.float64(np.array([SENSORPARAMS.get('a')] * 5 + [GO_ON_PARAMS.get('a')] +
-                                 [SENSORPARAMS.get('a')] * 2)),
-        "b": np.float64(np.array([SENSORPARAMS.get('b')] * 5 + [GO_ON_PARAMS.get('b')] +
-                                 [SENSORPARAMS.get('b')] * 2)),
-        "V_th": np.float64(np.array([SENSORPARAMS.get('v_thresh')] * 5 +
-                                    [GO_ON_PARAMS.get('v_thresh')] +
-                                    [SENSORPARAMS.get('v_thresh')] * 2)),
-        "Delta_T": np.float64(np.array([SENSORPARAMS.get('delta_T')] * 5 +
-                                       [GO_ON_PARAMS.get('delta_T')] +
-                                       [SENSORPARAMS.get('delta_T')] * 2)),
-        "C_m": np.float64(
-            np.array([SENSORPARAMS.get('cm')] * 5 + [GO_ON_PARAMS.get('cm')] +
-                     [SENSORPARAMS.get('cm')] * 2)),
-        "g_L": np.float64(np.array([g_L] * 8)),
-        "V_reset": np.float64(np.array([SENSORPARAMS.get('v_reset')] * 5 +
-                                       [GO_ON_PARAMS.get('v_reset')] +
-                                       [SENSORPARAMS.get('v_reset')] * 2)),
-        "tau_w": np.float64(np.array([SENSORPARAMS.get('tau_w')] * 5 +
-                                     [GO_ON_PARAMS.get('tau_w')] +
-                                     [SENSORPARAMS.get('tau_w')] * 2)),
-        "t_ref": np.float64(np.array([SENSORPARAMS.get('tau_refrac')] * 5 +
-                                     [GO_ON_PARAMS.get('tau_refrac')] +
-                                     [SENSORPARAMS.get('tau_refrac')] * 2)),
-        "V_peak": np.float64(np.array([SENSORPARAMS.get('v_spike')] * 5 +
-                                      [GO_ON_PARAMS.get('v_spike')] +
-                                      [SENSORPARAMS.get('v_spike')] * 2)),
-        "E_L": np.float64(np.array([SENSORPARAMS.get('v_rest')] * 5 +
-                                   [GO_ON_PARAMS.get('v_rest')] +
-                                   [SENSORPARAMS.get('v_rest')] * 2)),
-        "E_ex": np.float64(np.array([SENSORPARAMS.get('e_rev_E')] * 5 +
-                                    [GO_ON_PARAMS.get('e_rev_E')] +
-                                    [SENSORPARAMS.get('e_rev_E')] * 2)),
-        "E_in": np.float64(np.array([SENSORPARAMS.get('e_rev_I')] * 5 +
-                                    [GO_ON_PARAMS.get('e_rev_I')] +
-                                    [SENSORPARAMS.get('e_rev_I')] * 2)),
-        "tau_syn_E": np.float64(np.array([SENSORPARAMS.get('tau_syn_E')] * 5 +
-                                         [GO_ON_PARAMS.get('tau_syn_E')] +
-                                         [SENSORPARAMS.get('tau_syn_E')] * 2)),
-        "tau_syn_I": np.float64(np.array([SENSORPARAMS.get('tau_syn_I')] * 5 +
-                                         [GO_ON_PARAMS.get('tau_syn_I')] +
-                                         [SENSORPARAMS.get('tau_syn_I')] * 2)),
-        "excitatory": np.array([90, 90, 110, 110, 110, 110, 110, 110])
-    }
-
-    population = formatted_circuit['population']
-
-    # AdEx parameters are set
-    population.tset('v_thresh', formatted_circuit["V_th"])
-    population.tset('cm', formatted_circuit["C_m"] * 1e-3)  # pF->nF
-    population.tset('tau_m', formatted_circuit["C_m"] / formatted_circuit["g_L"])
-    population.tset('v_reset', formatted_circuit["V_reset"])
-    population.tset('tau_refrac', formatted_circuit["t_ref"])
-    population.tset('v_rest', formatted_circuit["E_L"])
-    population.tset('e_rev_E', formatted_circuit["E_ex"])
-    population.tset('e_rev_I', formatted_circuit["E_in"])
-    population.tset('tau_syn_E', formatted_circuit["tau_syn_E"])
-    population.tset('tau_syn_I', formatted_circuit["tau_syn_I"])
-
-    sim.initialize(population, 'v',
-                   population.get('v_rest'))
-
+    sim.initialize(population, v=population.get('v_rest'))
 
     logger.info("Circuit description: " + str(population.describe()))
+
     return population
 
 
 circuit = create_brain()
-
