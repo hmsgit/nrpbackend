hmsgit/nrpbackend

commit 9dfdf9409cc0b2b100c82cfae2bd98f369f8834a

[NRRPLT-6985] use copied sdf from storage instead of template one

hbp_nrp_cleserver/hbp_nrp_cleserver/server/CLEGazeboSimulationAssembly.py
@@ -491,7 +491,9 @@ class CLEGazeboSimulationAssembly(GazeboSimulationAssembly):
             if isCustom:
                 path = self._extract_robot_zip(modelTag)
             else:
-                path = find_file_in_paths(modelTag.value(), get_model_basepath())
+                path = find_file_in_paths(os.path.join(modelTag.robotId,
+                                                       os.path.basename(modelTag.value())),
+                                          get_model_basepath())
 
             if not path:
                 raise Exception("Could not find robot file: ".format(modelTag.value()))

hbp_nrp_cleserver/hbp_nrp_cleserver/server/_RobotCallHandler.py
@@ -104,7 +104,8 @@ class RobotCallHandler(object):
                 import json
                 try:    # pragma: no cover
                     # Hack the file name for the storage server. Replace spaces and slashes.
-                    # How do i know this? devine knowledge!
+                    # How do you know this? divine knowledge!
+                    # FIXME: get_custom_model should take a path, custom logic should be done there
                     requestFile = ('robots/' + zipRelPath).replace(' ', '%20').replace('/', '%2F')
                     # download zip from storage
                     data = client.get_custom_model(

commit 7e6e2cf2f2fbb98c74947c1d2a3cf730a5ed1f75

[NRRPLT-0000] fixed launching of previously cloned experiments. Compatibility issue introduced in 9dfdf94 [NRRPLT-6985]

hbp_nrp_cleserver/hbp_nrp_cleserver/server/CLEGazeboSimulationAssembly.py
@@ -494,9 +494,16 @@ class CLEGazeboSimulationAssembly(GazeboSimulationAssembly):
                 path = find_file_in_paths(os.path.join(modelTag.robotId,
                                                        os.path.basename(modelTag.value())),
                                           get_model_basepath())
+                # Perhaps it's a previously coned experiment? Try with modelTag.value() BUT
+                # only look into the simulation_directory, as DELETE robot REST call, if called,
+                # would delete this file
+                # TODO: backward compatibility code. Remove when we decide not to support anymore
+                if not path:
+                    path = find_file_in_paths(modelTag.value(), [self._simDir])
 
+            # still couldn't find the SDF, abort!
             if not path:
-                raise Exception("Could not find robot file: ".format(modelTag.value()))
+                raise Exception("Could not find robot file: {0}".format(modelTag.value()))
             self.robotManager.add_robot(
                 Robot(modelTag.robotId, path, modelTag.robotId, pose, isCustom)
             )